afc0ced68247609321b7f86ab064314831d249a1
Deena Sherin <sdeenasherin@gmail.com>
2026-01-19 15:30:28 +0530

    added gitigore file

--- Ticket_Raising_Backend/TicketClassLibrary/obj/Debug/net10.0/TicketClassLibrary.AssemblyInfo.cs
+++ Ticket_Raising_Backend/TicketClassLibrary/obj/Debug/net10.0/TicketClassLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5638ca73301f7455755bd497bc712aaccd7c5108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcaa370f2e1cbb0eff6bc1541732f880fa18bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
