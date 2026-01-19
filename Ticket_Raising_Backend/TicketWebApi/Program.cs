4e5b481dcabea06db453cbcdf0dd249c468dd158
Ramiya Sri S <ramiyasri28srs@gmail.com>
2026-01-19 21:47:01 +0530

    TicketPriorityController created

--- Ticket_Raising_Backend/TicketWebApi/Program.cs
+++ Ticket_Raising_Backend/TicketWebApi/Program.cs
@@ -1,8 +1,11 @@
+using TicketClassLibrary.Repos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ITicketPriorityRepository, EFTicketPriorityRepository>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
