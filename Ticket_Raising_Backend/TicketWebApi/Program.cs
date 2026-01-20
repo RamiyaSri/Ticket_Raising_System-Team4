77994a695c7bb0ab63c85a8da417ab9c0f0e0e2d
Deepa roshini <deeparoshini2004@gmail.com>
2026-01-20 09:17:44 +0530

    Ticket type webapi created

--- Ticket_Raising_Backend/TicketWebApi/Program.cs
+++ Ticket_Raising_Backend/TicketWebApi/Program.cs
@@ -1,4 +1,6 @@
+using TicketClassLibrary.Models;
 using TicketClassLibrary.Repos;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<ITicketPriorityRepository, EFTicketPriorityRepository>();
+builder.Services.AddScoped<ITicketTypeRepository, EFTicketTypeRepository>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
