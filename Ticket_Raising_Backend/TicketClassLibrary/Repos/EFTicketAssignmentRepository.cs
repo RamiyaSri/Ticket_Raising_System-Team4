dafc5f996c732e548f69b45833f632fdff179335
Sasticsaran Balasubramanian <Sasticbala1305@gmail.com>
2026-01-20 09:52:12 +0530

    Updated TicketAssignmentRepo for NameSpace

--- Ticket_Raising_Backend/TicketClassLibrary/Repos/EFTicketAssignmentRepository.cs
+++ Ticket_Raising_Backend/TicketClassLibrary/Repos/EFTicketAssignmentRepository.cs
@@ -1,7 +1,6 @@
 using System;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using TicketClassLibrary.Exceptions;
 using TicketClassLibrary.Models;
 
 namespace TicketClassLibrary.Repos;
