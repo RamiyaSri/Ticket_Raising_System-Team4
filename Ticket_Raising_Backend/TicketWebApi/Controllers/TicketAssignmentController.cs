7154a31c31db03bb07109a2fb6165ed0c36763e9
Sasticsaran Balasubramanian <Sasticbala1305@gmail.com>
2026-01-20 14:19:55 +0530

    Authorize is updated

--- Ticket_Raising_Backend/TicketWebApi/Controllers/TicketAssignmentController.cs
+++ Ticket_Raising_Backend/TicketWebApi/Controllers/TicketAssignmentController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TicketClassLibrary.Models;
@@ -6,6 +7,7 @@ using TicketClassLibrary.Repos;
 namespace TicketWebApi.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class TicketAssignmentController : ControllerBase
     {
