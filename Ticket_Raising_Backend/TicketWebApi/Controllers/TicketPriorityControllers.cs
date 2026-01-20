59e85de23b4d0fc7d979b880f4dbc5ee94417bb9
Ramiya Sri S <ramiyasri28srs@gmail.com>
2026-01-20 12:01:46 +0530

    Authorization

--- Ticket_Raising_Backend/TicketWebApi/Controllers/TicketPriorityControllers.cs
+++ Ticket_Raising_Backend/TicketWebApi/Controllers/TicketPriorityControllers.cs
@@ -7,6 +7,7 @@ namespace TicketWebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PrioritiesController : ControllerBase
     {
         ITicketPriorityRepository priorityRepo;
--- Ticket_Raising_Backend/TicketWebApi/Program.cs
+++ Ticket_Raising_Backend/TicketWebApi/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 using TicketClassLibrary.Repos;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,23 +16,67 @@ builder.Services.AddScoped<IEmployeeRepository, EFEmployeeRepository>();
 builder.Services.AddScoped<ITicketAssignmentRepository, EFTicketAssignmentRepository>();
 builder.Services.AddScoped<ITicketCommentRepository, EFTicketCommentRepository>();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authentication using Bearer scheme"
+    });
+ 
+    options.AddSecurityRequirement(doc => new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecuritySchemeReference("Bearer", doc),
+            new List<string>()
+        }
+    });
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters =
+        new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = "https://www.team4.com",
+            ValidAudience = "https://www.team4.com",
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(
+                       "My name is not important but my key is very important!"
+                    )
+                )
+        };
+});
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.MapOpenApi();
 }
-
+app.UseCors("MyPolicy");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+ 
 app.MapControllers();
-
+app.UseSwagger();
+app.UseSwaggerUI();
+ 
 app.Run();
