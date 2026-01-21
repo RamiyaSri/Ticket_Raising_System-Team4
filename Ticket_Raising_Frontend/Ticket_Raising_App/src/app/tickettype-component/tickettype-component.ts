60ba3b16e925d11917c3ddd887791455ab282e7f
Ramiya Sri S <ramiyasri28srs@gmail.com>
2026-01-21 18:56:05 +0530

    Added Bootstrap

--- Ticket_Raising_Frontend/Ticket_Raising_App/src/app/tickettype-component/tickettype-component.ts
+++ Ticket_Raising_Frontend/Ticket_Raising_App/src/app/tickettype-component/tickettype-component.ts
@@ -1,8 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { TicketTypeService } from '../tickettype-service';
-import { TicketType } from '../../Models/TicketType';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+ 
+import { TicketTypeService } from '../tickettype-service';
+import { TicketPriorityService } from '../ticketpriority-service';
+ 
+import { TicketType } from '../../Models/TicketType';
+import { TicketPriority } from '../../Models/TicketPriority';
  
 @Component({
   selector: 'app-ticket-type-component',
@@ -13,36 +17,52 @@ import { FormsModule } from '@angular/forms';
 export class TicketTypeComponent {
  
   ticketTypeSvc: TicketTypeService = inject(TicketTypeService);
-  ticketTypes: TicketType[];
+  prioritySvc: TicketPriorityService = inject(TicketPriorityService);
+ 
   ticketType: TicketType;
+  ticketTypes: TicketType[];
+ 
+  priorities: TicketPriority[];   
+  priorityId: string;      
+ 
   errMsg: string;
-  priorityId: string;   
  
   constructor() {
     this.ticketTypes = [];
-    this.ticketType = new TicketType("", "", "", "");
-    this.errMsg = "";
+    this.priorities = [];
     this.priorityId = "";
+    this.errMsg = "";
+    this.ticketType = new TicketType("", "", "", "");
+ 
     this.showAllTicketTypes();
+    this.loadPriorities();
   }
+ 
+ 
+  loadPriorities() {
+    this.prioritySvc.getAllPriorities().subscribe({
+      next: (response: TicketPriority[]) => {
+        this.priorities = response;
+        this.errMsg = "";
+      },
+      error: (err) => this.errMsg = err.message
+    });
+  }
+ 
  
   showAllTicketTypes() {
     this.ticketTypeSvc.getAllTicketTypes().subscribe({
       next: (response: any) => {
         this.ticketTypes = response;
-        console.log(response);
         this.errMsg = "";
       },
-      error: (err) => { 
-        this.errMsg = err.message; 
-        console.log(err); 
-      }
+      error: (err) => this.errMsg = err.message
     });
   }
  
   saveTicketType() {
     this.ticketTypeSvc.addTicketType(this.ticketType).subscribe({
-      next: (response: any) => {
+      next: () => {
         alert("New Ticket Type added");
         this.errMsg = "";
         this.showAllTicketTypes();
@@ -67,7 +87,7 @@ export class TicketTypeComponent {
  
   updateTicketType() {
     this.ticketTypeSvc.updateTicketType(this.ticketType.ticketTypeId, this.ticketType).subscribe({
-      next: (response: any) => {
+      next: () => {
         alert("Ticket Type updated successfully");
         this.errMsg = "";
         this.showAllTicketTypes();
@@ -78,7 +98,7 @@ export class TicketTypeComponent {
  
   deleteTicketType() {
     this.ticketTypeSvc.deleteTicketType(this.ticketType.ticketTypeId).subscribe({
-      next: (response: any) => {
+      next: () => {
         alert("Ticket Type deleted successfully");
         this.errMsg = "";
         this.showAllTicketTypes();
@@ -87,21 +107,19 @@ export class TicketTypeComponent {
     });
   }
  
-showTicketTypesByPriority() {
-  console.log("Priority Id entered:", this.priorityId);
+  showTicketTypesByPriority() {
+    console.log("Selected Priority Id:", this.priorityId);
  
-  this.ticketTypeSvc.getTicketTypesByPriority(this.priorityId).subscribe({
-    next: (response: any) => {
-      console.log("Response from API:", response);
-      this.ticketTypes = response;
-      this.errMsg = "";
-    },
-    error: (err) => {
-      console.log("API Error:", err);
-      this.errMsg = err.error;
-    }
-  });
+    this.ticketTypeSvc.getTicketTypesByPriority(this.priorityId).subscribe({
+      next: (response: any) => {
+        console.log("Filtered Ticket Types:", response);
+        this.ticketTypes = response;
+        this.errMsg = "";
+      },
+      error: (err) => {
+        console.log("API Error:", err);
+        this.errMsg = err.error || err.message;
+      }
+    });
+  }
 }
-}
- 
- 
